c81a8d1eedd99ce16a47c8efbaa6c2e4bac1090c
smobx1 <mnmoldoveanu@gmail.com>
2016-05-04 16:55:26 +0300

    s

--- js/main.js
+++ js/main.js
@@ -43,11 +43,11 @@ var replayclickable = false;
 
 //sounds
 var volume = 30;
-// var soundJump = new buzz.sound("assets/sounds/sfx_wing.mp3");         // jump()
+var soundJump = new buzz.sound("/assets/sounds/sfx_wing.mp3");         // jump()
 var soundScore = new buzz.sound("assets/sounds/sfx_point.mp3");       // trece prin pipes()
 var soundHit = new buzz.sound("assets/sounds/cristi-iii.mp3");           // hit()
 var soundDie = new buzz.sound("assets/sounds/sfx_die.mp3");           // 1s dupa hit()
-// var soundSwoosh = new buzz.sound("assets/sounds/sfx_swooshing.mp3");  // la      start() (hu)
+var soundSwoosh = new buzz.sound("assets/sounds/sfx_swooshing.mp3");  // la      start() (hu)
 buzz.all().setVolume(volume);
 
 //loops
