2dd0e13389289a83bafbfdb5036efd6ba044a7a3
smobx1 <mnmoldoveanu@gmail.com>
2016-05-12 23:17:34 +0300

    bac

--- admob_simple.js
+++ admob_simple.js
@@ -3,17 +3,17 @@ var admobid = {};
 // TODO: replace the following ad units with your own
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
-    // banner: 'ca-app-pub-2789022727093400/7481471570',
-    interstitial: 'ca-app-pub-2789022727093400/7341870772'
+    // banner: 'ca-app-pub-6869992474017983/9375997553',
+    interstitial: 'ca-app-pub-2789022727093400/5194583579'
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
-    // banner: 'ca-app-pub-2789022727093400/7481471570',
-    interstitial: 'ca-app-pub-2789022727093400/7341870772'
+    // banner: 'ca-app-pub-6869992474017983/4806197152',
+    interstitial: 'ca-app-pub-2789022727093400/7090119175'
   };
 } else {
   admobid = { // for Windows Phone
-    // banner: 'ca-app-pub-2789022727093400/7481471570',
+    // banner: 'ca-app-pub-6869992474017983/8878394753',
     // interstitial: 'ca-app-pub-6869992474017983/1355127956'
   };
 }
