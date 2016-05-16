bf124bbf8a619d28db7ceea4b4add996e57ac4f1
smobx1 <mnmoldoveanu@gmail.com>
2016-05-16 20:12:33 +0300

    s

--- admob_simple.js
+++ admob_simple.js
@@ -24,24 +24,24 @@ function initApp() {
   if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
 
 
-  setTimeout(function() {
-        // this will create a banner on startup
-        AdMob.createBanner( {
-          adId: admobid.banner,
-          position: AdMob.AD_POSITION.BOTTOM_CENTER,
-          // isTesting: true, // TODO: remove this line when release
-          overlap: false,
-          offsetTopBar: false,
-          bgColor: 'black'
-        } );
-
-        // this will load a full screen ad on startup
-        AdMob.prepareInterstitial({
-          adId: admobid.interstitial,
-          // isTesting: true, // TODO: remove this line when release
-          autoShow: true
-        });
-  }, 2000);
+  // setTimeout(function() {
+  //       // this will create a banner on startup
+  //       AdMob.createBanner( {
+  //         adId: admobid.banner,
+  //         position: AdMob.AD_POSITION.BOTTOM_CENTER,
+  //         // isTesting: true, // TODO: remove this line when release
+  //         overlap: false,
+  //         offsetTopBar: false,
+  //         bgColor: 'black'
+  //       } );
+  //
+  //       // this will load a full screen ad on startup
+  //       AdMob.prepareInterstitial({
+  //         adId: admobid.interstitial,
+  //         // isTesting: true, // TODO: remove this line when release
+  //         autoShow: true
+  //       });
+  // }, 2000);
 
     // this will create a banner on startup
           AdMob.createBanner( {
